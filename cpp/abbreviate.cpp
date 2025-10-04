c5f3514fbbeef0105d10bdd864f9087059693457
Shivam <shivamdubey252005@outlook.com>
2025-10-04 19:43:51 +0530

    newsoln

--- cpp/abbreviate.cpp
+++ cpp/abbreviate.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
 #include <vector>
 #include <string>
+typedef struct inputstrings
+{
+    std::string input;
+}array;
 int main()
 {
-    std::vector<std::string> inputvalues;
     int val;
+    std::string temp="";
+    std::string temp2;
     std::cin>>val;
-    for(i=0;i<val;i++)
+    std::vector<array> inputvalues(val);
+    for(int i=0;i<val;i++)
     {
-        std::string temp;
-        std::cin>>inputvalues[i];
-        if(inputvalues[i].length()>10)
+        std::cin>>inputvalues[i].input;
+        if(inputvalues[i].input.length()>10)
         {
-            temp=inputvalues[i][0];
-            temp.append(std::string(inputvalues[i].length()-3));
-            temp.append(inputvalues[i][inputvalues[i].length()-1]);
-            inputvalues
+            temp="";
+            temp=temp+inputvalues[i].input[0];
+            temp2=std::to_string(inputvalues[i].input.length()-2);
+            temp.append(temp2);
+            temp=temp+(inputvalues[i].input[inputvalues[i].input.length()-1]);
+            inputvalues[i].input=temp;
         }
+    }
+    for(int i=0;i<val;i++)
+    {
+        std::cout<<inputvalues[i].input<<"\n";
     }
 }
